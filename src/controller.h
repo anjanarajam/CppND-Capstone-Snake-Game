1982b6eba877cb076d8cb84da660deb15a7d2d02
Anjana Rajam <64962809+anjanarajam@users.noreply.github.com>
2020-11-30 21:15:38 +0100

    Added child class to a controller with virtual function

--- src/controller.h
+++ src/controller.h
@@ -4,12 +4,20 @@
 #include "snake.h"
 
 class Controller {
- public:
-  void HandleInput(bool &running, Snake &snake) const;
+public:
+    //Anjana..
+    //Making this function virtual
+    virtual void HandleInput(bool& running, Snake& snake) const;
 
- private:
-  void ChangeDirection(Snake &snake, Snake::Direction input,
-                       Snake::Direction opposite) const;
+    //private:
+protected:
+    void ChangeDirection(Snake& snake, Snake::Direction input,
+        Snake::Direction opposite) const;
 };
 
+
+//Anjana.. Adding child class to controller
+class MouseWheelController : public Controller {
+    void HandleInput(bool& running, Snake& snake) const override;
+};
 #endif
